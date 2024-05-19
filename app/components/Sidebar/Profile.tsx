2038f9efdda9ce20c429dcd319a3c6d647ee1e07
MatheusGSantos <mths.gm@gmail.com>
2024-05-19 18:09:01 -0300

    feat: Add ghost variant to Button component and update usage in FileList and Profile

--- app/components/Sidebar/Profile.tsx
+++ app/components/Sidebar/Profile.tsx
@@ -1,8 +1,9 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '@/components/Button'
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img
         src="https://github.com/diego3g.png"
         className="h-10 w-10 rounded-full"
@@ -16,9 +17,9 @@ export function Profile() {
           diego@rocketseat.com.br
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
