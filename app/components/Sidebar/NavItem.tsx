861ad13bb57c6a93934aae983206786dcbd442db
MatheusGSantos <mths.gm@gmail.com>
2024-05-26 22:53:50 -0300

    refactor: Update Button, Input, Logo, NavItem, Profile, and UsedSpaceWidget components for dark mode support

--- app/components/Sidebar/NavItem.tsx
+++ app/components/Sidebar/NavItem.tsx
@@ -13,10 +13,10 @@ export function NavItem({ title, icon: Icon }: NavItemProps) {
       className="group flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50"
     >
       <Icon className="h-5 w-5 text-zinc-500" />
-      <span className="font-medium text-zinc-700 group-hover:text-violet-500">
+      <span className="font-medium text-zinc-700 group-hover:text-violet-500  dark:hover:bg-zinc-800">
         {title}
       </span>
-      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-300" />
+      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-300 dark:text-zinc-600" />
     </a>
   )
 }
